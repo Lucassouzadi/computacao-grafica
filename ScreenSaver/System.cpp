5842e35236b8cdb72d80582b1ac2950c7930782c
Lucas Souza Dias <lucas.dias@cwi.com.br>
2021-08-22 03:15:09 -0300

    Add ScreenSaver implementation

--- ScreenSaver/System.cpp
+++ ScreenSaver/System.cpp
@@ -1,5 +1,5 @@
 #include "System.h"
-
+constexpr auto PI = 3.14159265;
 
 
 System::System()
@@ -77,61 +77,108 @@ void System::Run()
 
 	coreShader.Use();
 	coreShader.LoadTexture("images/woodTexture.jpg", "texture1", "woodTexture");
-
+	
+	GLfloat translate[] = { 
+		1.0f, 0.0f, 0.0f, 0.0f,  // 1ª coluna 
+		0.0f, 1.0f, 0.0f, 0.0f,  // 2ª coluna 
+		0.0f, 0.0f, 1.0f, 0.0f,  // 3ª coluna 
+		0.0f, 0.0f, 0.0f, 1.0f // 4ª coluna
+	};
 	GLfloat vertices[] =
 	{
-		// Positions         // Textures
-
-		 0.5f,  0.5f, 0.0f,   1.0f, 1.0f, // Top Right
-		 0.5f, -0.5f, 0.0f,   1.0f, 0.0f, // Bottom Right
-		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, // Bottom Left
-
-		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, // Bottom Left
-		-0.5f,  0.5f, 0.0f,   0.0f, 1.0f, // Top Left
-		 0.5f,  0.5f, 0.0f,   1.0f, 1.0f, // Top Right
+		0.0f, 0.3f, 0.0f,	// Top
+		0.3f,  -0.3f, 0.0f,	// Bottom Right
+		-0.3f, -0.3f, 0.0f	// Bottom Left
+	};
+	GLfloat colors[] =
+	{
+		1.0f, 0.0f, 0.0f, // Top
+		0.0f, 1.0f, 0.0f, // Bottom Right
+		0.0f, 0.0f, 1.0f  // Bottom Left
 	};
 
-	GLuint VBO, VAO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
+	GLuint positionVBO, colorVBO, triangleVAO;
+	glGenVertexArrays(1, &triangleVAO);
+	glGenBuffers(1, &positionVBO);
+	glGenBuffers(1, &colorVBO);
 
 	// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
-	glBindVertexArray(VAO);
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindVertexArray(triangleVAO);
 
 	// Position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
+	glBindBuffer(GL_ARRAY_BUFFER, positionVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
 
-	// Texture attribute
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	// Color attribute
+	glBindBuffer(GL_ARRAY_BUFFER, colorVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(1);
 
 	glBindVertexArray(0); // Unbind VAO
+
+	int translateLocation = glGetUniformLocation(coreShader.program, "translate");
+
+	float speed = 1.0f;
+	float directionX = 1.0f;
+	float directionY = 1.0f;
+	float lastPositionX = 0.0f;
+	float lastPositionY = 0.0f;
+	float angle = 40.0f; // Angle in degrees
+
+	double seno = sin(angle * PI / 180);
+	double cosseno = cos(angle * PI / 180);
+
+	printf("seno %f\n", seno);
+	printf("cosseno %f\n", cosseno);
 
 	while (!glfwWindowShouldClose(window)) {
 
 		glfwPollEvents();
 
-#pragma region Input Handling
-
 		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
 			glfwSetWindowShouldClose(window, GLFW_TRUE);
 		}
 
-#pragma endregion
-
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+
+		static double previousSeconds = glfwGetTime();
+		double currentSeconds = glfwGetTime();
+		double elapsedSeconds = currentSeconds - previousSeconds;
+		previousSeconds = currentSeconds;
+
+		for (int vert = 0; vert < 3; vert++) {
+			float vertX = lastPositionX+vertices[vert * 3];
+			float vertY = lastPositionY+vertices[vert * 3 + 1];
+
+			if (vertX > 1.0f) {			// Right border colision
+				directionX = -1.0f; 
+			}
+			else if (vertX < -1.0f) {	// Left border colision
+				directionX = 1.0f;
+			}
+			if (vertY > 1.0f) {			// Top border colision
+				directionY = -1.0f;
+			}
+			else if (vertY < -1.0f) {	// Bottom border colision
+				directionY = 1.0f;
+			}
+		}
+
+		lastPositionX += elapsedSeconds * speed * cosseno * directionX;
+		translate[12] = lastPositionX;
+		lastPositionY += elapsedSeconds * speed * seno * directionY;
+		translate[13] = lastPositionY;
+
 		coreShader.Use();
+		glUniformMatrix4fv(translateLocation, 1, GL_FALSE, translate);
 
-		coreShader.UseTexture("woodTexture");
-
-		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glBindVertexArray(triangleVAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glBindVertexArray(0);
 
 
